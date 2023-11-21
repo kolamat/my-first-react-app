e43702fde657c1b6e84651cdb322f1cec9a4386e
kolamat <132449653+kolamat@users.noreply.github.com>
2023-11-21 02:22:11 -0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo">
             <i class="fa-sharp fa-solid fa-p">ortFolio</i>
-          </link>
+          </Link>
         </div>
       </nav>
     </>
